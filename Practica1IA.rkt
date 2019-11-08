6cf6d61daf2b36691cfaa30acd59f32d8b40d644
JPovedanoR <38103406+JPovedanoR@users.noreply.github.com>
2019-11-08 14:49:19 +0100

    Update Practica1IA.rkt

--- Practica1IA.rkt
+++ Practica1IA.rkt
@@ -56,7 +56,7 @@
   (cond
     [(> 3 posicionx)
      (cond
-       [(> 3 posiciony)1]
+       [(> 3 posiciony)(equal? #f (member number (construirCuadrante 1 board)) #f) #t]
        [(and (< 3 posiciony)(> 6 posiciony))2]
        [(< 6 posiciony)3])
     ]
@@ -76,24 +76,109 @@
 
    )
 )
-(define(crearCuadrante numeroCuadrante sudoku)
+
+(define(construirCuadrante numeroCuadrante board)
   (cond
-    [(= numeroCuadrante 1)]
-    [(= numeroCuadrante 2)]
-    [(= numeroCuadrante 3)]
-    [(= numeroCuadrante 4)]
-    [(= numeroCuadrante 5)]
-    [(= numeroCuadrante 6)]
-    [(= numeroCuadrante 7)]
-    [(= numeroCuadrante 8)]
-    [(= numeroCuadrante 9)]
+    [(= numeroCuadrante 1)(list (list-ref (car(listaConstruir3PrimerasFilas board 0))0)
+                                (list-ref (car(listaConstruir3PrimerasFilas board 0))1)
+                                (list-ref (car(listaConstruir3PrimerasFilas board 0))2)
+                                (list-ref (car(listaConstruir3PrimerasFilas board 1))0)
+                                (list-ref (car(listaConstruir3PrimerasFilas board 1))1)
+                                (list-ref (car(listaConstruir3PrimerasFilas board 1))2)
+                                (list-ref (car(listaConstruir3PrimerasFilas board 2))0)
+                                (list-ref (car(listaConstruir3PrimerasFilas board 2))1)
+                                (list-ref (car(listaConstruir3PrimerasFilas board 2))2))]
+    
+    [(= numeroCuadrante 2)(list (list-ref (car(listaConstruir3PrimerasFilas board 0))3)
+                                (list-ref (car(listaConstruir3PrimerasFilas board 0))4)
+                                (list-ref (car(listaConstruir3PrimerasFilas board 0))5)
+                                (list-ref (car(listaConstruir3PrimerasFilas board 1))3)
+                                (list-ref (car(listaConstruir3PrimerasFilas board 1))4)
+                                (list-ref (car(listaConstruir3PrimerasFilas board 1))5)
+                                (list-ref (car(listaConstruir3PrimerasFilas board 2))3)
+                                (list-ref (car(listaConstruir3PrimerasFilas board 2))4)
+                                (list-ref (car(listaConstruir3PrimerasFilas board 2))5))]
+    
+    [(= numeroCuadrante 3)(list (list-ref (car(listaConstruir3PrimerasFilas board 0))6)
+                                (list-ref (car(listaConstruir3PrimerasFilas board 0))7)
+                                (list-ref (car(listaConstruir3PrimerasFilas board 0))8)
+                                (list-ref (car(listaConstruir3PrimerasFilas board 1))6)
+                                (list-ref (car(listaConstruir3PrimerasFilas board 1))7)
+                                (list-ref (car(listaConstruir3PrimerasFilas board 1))8)
+                                (list-ref (car(listaConstruir3PrimerasFilas board 2))6)
+                                (list-ref (car(listaConstruir3PrimerasFilas board 2))7)
+                                (list-ref (car(listaConstruir3PrimerasFilas board 2))8))]
+    
+    [(= numeroCuadrante 4)(list (list-ref (car(listaConstruir3FilasDelMedio board 3))0)
+                                (list-ref (car(listaConstruir3FilasDelMedio board 3))1)
+                                (list-ref (car(listaConstruir3FilasDelMedio board 3))2)
+                                (list-ref (car(listaConstruir3FilasDelMedio board 4))0)
+                                (list-ref (car(listaConstruir3FilasDelMedio board 4))1)
+                                (list-ref (car(listaConstruir3FilasDelMedio board 4))2)
+                                (list-ref (car(listaConstruir3FilasDelMedio board 5))0)
+                                (list-ref (car(listaConstruir3FilasDelMedio board 5))1)
+                                (list-ref (car(listaConstruir3FilasDelMedio board 5))2))]
+    
+    [(= numeroCuadrante 5)(list (list-ref (car(listaConstruir3FilasDelMedio board 3))3)
+                                (list-ref (car(listaConstruir3FilasDelMedio board 3))4)
+                                (list-ref (car(listaConstruir3FilasDelMedio board 3))5)
+                                (list-ref (car(listaConstruir3FilasDelMedio board 4))3)
+                                (list-ref (car(listaConstruir3FilasDelMedio board 4))4)
+                                (list-ref (car(listaConstruir3FilasDelMedio board 4))5)
+                                (list-ref (car(listaConstruir3FilasDelMedio board 5))3)
+                                (list-ref (car(listaConstruir3FilasDelMedio board 5))4)
+                                (list-ref (car(listaConstruir3FilasDelMedio board 5))5))]
+    
+    [(= numeroCuadrante 6)(list (list-ref (car(listaConstruir3FilasDelMedio board 3))6)
+                                (list-ref (car(listaConstruir3FilasDelMedio board 3))7)
+                                (list-ref (car(listaConstruir3FilasDelMedio board 3))8)
+                                (list-ref (car(listaConstruir3FilasDelMedio board 4))6)
+                                (list-ref (car(listaConstruir3FilasDelMedio board 4))7)
+                                (list-ref (car(listaConstruir3FilasDelMedio board 4))8)
+                                (list-ref (car(listaConstruir3FilasDelMedio board 5))6)
+                                (list-ref (car(listaConstruir3FilasDelMedio board 5))7)
+                                (list-ref (car(listaConstruir3FilasDelMedio board 5))8))]
+    
+    [(= numeroCuadrante 7)(list (list-ref (car(listaConstruir3UltimasFilas board 6))0)
+                                (list-ref (car(listaConstruir3UltimasFilas board 6))1)
+                                (list-ref (car(listaConstruir3UltimasFilas board 6))2)
+                                (list-ref (car(listaConstruir3UltimasFilas board 7))0)
+                                (list-ref (car(listaConstruir3UltimasFilas board 7))1)
+                                (list-ref (car(listaConstruir3UltimasFilas board 7))2)
+                                (list-ref (car(listaConstruir3UltimasFilas board 8))0)
+                                (list-ref (car(listaConstruir3UltimasFilas board 8))1)
+                                (list-ref (car(listaConstruir3UltimasFilas board 8))2))]
+    
+    [(= numeroCuadrante 8)(list (list-ref (car(listaConstruir3UltimasFilas board 6))3)
+                                (list-ref (car(listaConstruir3UltimasFilas board 6))4)
+                                (list-ref (car(listaConstruir3UltimasFilas board 6))5)
+                                (list-ref (car(listaConstruir3UltimasFilas board 7))3)
+                                (list-ref (car(listaConstruir3UltimasFilas board 7))4)
+                                (list-ref (car(listaConstruir3UltimasFilas board 7))5)
+                                (list-ref (car(listaConstruir3UltimasFilas board 8))3)
+                                (list-ref (car(listaConstruir3UltimasFilas board 8))4)
+                                (list-ref (car(listaConstruir3UltimasFilas board 8))5))]
+    
+    [(= numeroCuadrante 9)(list (list-ref (car(listaConstruir3UltimasFilas board 6))6)
+                                (list-ref (car(listaConstruir3UltimasFilas board 6))7)
+                                (list-ref (car(listaConstruir3UltimasFilas board 6))8)
+                                (list-ref (car(listaConstruir3UltimasFilas board 7))6)
+                                (list-ref (car(listaConstruir3UltimasFilas board 7))7)
+                                (list-ref (car(listaConstruir3UltimasFilas board 7))8)
+                                (list-ref (car(listaConstruir3UltimasFilas board 8))6)
+                                (list-ref (car(listaConstruir3UltimasFilas board 8))7)
+                                (list-ref (car(listaConstruir3UltimasFilas board 8))8))]
     )
   )
-(define(listaConstruir sudoku columna)(cond [(> columna -1) (cons (listaConstruir sudoku (- columna 1)) (list-ref sudoku columna))]))
-(cdr(car(listaConstruir board 2)))
-(listaConstruir board 2)
+(define(listaConstruir3PrimerasFilas sudoku columna)(cond [(< columna 3) (cons (list-ref sudoku columna) (listaConstruir3PrimerasFilas sudoku (+ columna 1)) )]))
+(define(listaConstruir3FilasDelMedio sudoku columna)(cond [(< columna 6) (cons (list-ref sudoku columna) (listaConstruir3FilasDelMedio sudoku (+ columna 1)) )]))
+(define(listaConstruir3UltimasFilas sudoku columna)(cond [(< columna 9) (cons (list-ref sudoku columna) (listaConstruir3UltimasFilas sudoku (+ columna 1)) )]))
 ;(numberInListC 3 board 3 5)
-
+(construirCuadrante 5 board)
+;(car(cdr(cdr(listaConstruir3PrimerasFilas board 0))))
+;(listaConstruir3FilasDelMedio board 3);(listaConstruir3UltimasFilas board 6)
+;(define(construirPrimerCuadrante board)(list (list-ref (car(listaConstruir3UltimasFilas board 6))0) (list-ref (car(listaConstruir3UltimasFilas board 6))1) (list-ref (car(listaConstruir3UltimasFilas board 6))2) (list-ref (car(listaConstruir3UltimasFilas board 7))0) (list-ref (car(listaConstruir3UltimasFilas board 7))1) (list-ref (car(listaConstruir3PrimerasFilas board 7))2) (list-ref (car(listaConstruir3PrimerasFilas board 8))0) (list-ref (car(listaConstruir3PrimerasFilas board 8))1) (list-ref (car(listaConstruir3PrimerasFilas board 8))2)))
+;(construirPrimerCuadrante board)
 ;(define primeralinea '(1 2 3 4 5 6 0 8 9)) 
 (define (firstZeroRow lista)
   (for/first ([i 9]
