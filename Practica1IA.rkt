a32f669390fa7d8e720bea9f47ad31cd3e450a59
JavierBrRuiz <javier.brenes@edu.uah.es>
2019-12-01 19:45:25 +0100

    subido al menos sudoku test19

--- Practica1IA.rkt
+++ Practica1IA.rkt
@@ -494,5 +494,15 @@ Retorno: se visualiza en pantalla o bien el sudoku solucion o la secuencia de pa
     (0 3 2 0 0 0 6 0 0)
     (0 0 0 0 0 0 0 0 0)))
 
+(define test19
+  '((0 0 0 0 0 8 0 0 0)
+    (3 0 8 0 0 0 7 0 0)
+    (2 9 4 7 0 0 0 8 1)
+    (4 0 0 8 6 0 0 7 0)
+    (0 0 0 0 0 0 0 0 0)
+    (0 7 0 0 9 2 0 0 6)
+    (6 5 0 0 0 1 2 4 3)
+    (0 0 3 0 0 0 8 0 9)
+    (0 0 0 5 0 0 0 0 0)))
 
 
